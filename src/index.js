e5a7e079b9373317fe72d8d5eed30765936f4792
SirIsaacNeutron <dbadir@cox.net>
2023-02-07 22:31:25 -0800

    Add CSS file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("testing testing 1 2 3")
+// This applies CSS classes since webpack will basically put all of this CSS file
+// into a <style> tag
+import "./styles.css"
+
+const content = document.getElementById("content")
+
+function createNavBar() {
+    const nav = doucment.createElement("nav")
+
+    const navHeader = document.createElement("h1")
+    navHeader.textContent = "Pizza Palace"
+
+    content.appendChild(nav)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,12 @@ module.exports = {
             template: "src/template.html"
         }),
     ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 }
