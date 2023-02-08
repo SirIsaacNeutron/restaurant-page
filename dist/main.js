4f05d4ec32df6fa0d9cfead3ae1b3ebea84b7da1
SirIsaacNeutron <dbadir@cox.net>
2023-02-07 22:16:46 -0800

    Change webpack to dev mode

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,9 @@
-console.log("testing testing 1 2 3");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("testing testing 1 2 3")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG9DIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwidGVzdGluZyB0ZXN0aW5nIDEgMiAzXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
